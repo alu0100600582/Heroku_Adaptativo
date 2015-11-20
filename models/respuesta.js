fafd8d67d6d4dc5137924e6049a91b27b030a15a
Francisco Mendoza González <alu0100600582@ull.edu.es>
2015-11-20 01:46:20 +0100

    Modificaciones pequeñas

--- models/respuesta.js
+++ models/respuesta.js
@@ -6,7 +6,8 @@ function Respuesta(respuesta){
 
   if(typeof(this.respuesta) === 'string' || typeof(this.respuesta) === 'number') {
     return function(x){return x === respuesta;};
-  }else if (this.respuesta.__proto__.toString() === '/(?:)/') {
+  }
+  else if (this.respuesta.__proto__.toString() === '/(?:)/') {
     return function(x){return x.match(respuesta);};
   }
   else if (this.respuesta.__proto__.toString() === "function () {}") {
