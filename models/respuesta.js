2d0f97be4bc207f16da633689a7ee0f68e03474c
Francisco Mendoza González <alu0100600582@ull.edu.es>
2015-11-20 01:39:37 +0100

    Creando clase Respuesta

--- models/respuesta.js
+++ models/respuesta.js
@@ -7,7 +7,6 @@ function Respuesta(respuesta){
   if(typeof(this.respuesta) === 'string' || typeof(this.respuesta) === 'number') {
     return function(x){return x === respuesta;};
   }else if (this.respuesta.__proto__.toString() === '/(?:)/') {
-    // var aux = this.respuesta;
     return function(x){return x.match(respuesta);};
   }
   else if (this.respuesta.__proto__.toString() === "function () {}") {
