ba09ee53047e9666d3ca7df0f1b551cce6d5ae0b
Francisco Mendoza González <alu0100600582@ull.edu.es>
2015-11-20 03:01:38 +0100

    Intentando Arreglar errores

--- models/pregunta.js
+++ models/pregunta.js
@@ -4,14 +4,15 @@ function Pregunta(enunciado){
   this.enunciado = enunciado;
 }
 
+
 function PreguntaLarga(enunciado){
   Pregunta.call(this,enunciado);
-  this.entrada = "<textarea type='text' name='respuesta'></textarea>";
+  this.entrada = "<textarea name='respuesta' rows='2' cols='50'></textarea>";
 }
 
 function PreguntaCorta(enunciado){
   Pregunta.call(this,enunciado);
-  this.entrada = "<input type='text' name='respuesta' >";
+  this.entrada = "<input type='text' name='respuesta'>";
 }
 
 
